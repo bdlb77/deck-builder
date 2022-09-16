63eec19bb058dc1a5cf86245c37137d27498e21e
bryan leighton <bryan.leigh77@gmail.com>
2022-09-16 16:55:20 +0200

    init webpack ts react styled

---
+++ src/renderer/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App: React.FC = () => {
+  return (
+    <h2>Hello Form React!</h2>
+  )
+}
+
+export default App;
---
+++ src/renderer/renderer.tsx
@@ -0,0 +1,29 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+
+console.log('👋 This message is being logged by "renderer.js", included via webpack');
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+import { createGlobalStyle } from 'styled-components';
+
+
+const GlobalStyle = createGlobalStyle(({ theme }) => ({
+  "html, body, #root": {
+    height: "100%",
+    fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans- serif",
+    margin: "auto",
+    maxWidth: "38rem",
+    padding: "2rem",
+  },
+
+}));
+
+
+root.render (
+  <>
+  <GlobalStyle />
+  <App />
+  </>
+)
